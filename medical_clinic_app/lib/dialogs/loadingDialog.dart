4fa07bce33bd6f96ae5cc64d89b99a680d4663a2
anuj <anujrana2392001@gmail.com>
2024-01-30 16:45:37 +0530

    first

---
+++ medical_clinic_app/lib/dialogs/loadingDialog.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class loadingDialog extends StatefulWidget {
+  const loadingDialog({super.key});
+
+  @override
+  State<loadingDialog> createState() => _loadingDialogState();
+}
+
+class _loadingDialogState extends State<loadingDialog> {
+  @override
+  Widget build(BuildContext context) {
+    return  Dialog(shadowColor: Colors.transparent,backgroundColor: Colors.transparent,
+        child: Container(
+          color: Colors.transparent,
+          height: 300,
+          width: 100,
+          child: Lottie.asset("assets/loaders/loading.json"),
+        ));
+  }
+}
